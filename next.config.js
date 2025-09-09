202f8220014c31ef7f4fd28319793f56d1a76787
Masud Parvaz <108426788+masud667@users.noreply.github.com>
2025-09-09 20:31:37 +0600

    added next config fil6e

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.(png|jpe?g|gif|svg)$/i,
-      type: "asset/resource",
-    });
-    return config;
-  },
-};
+
 
 module.exports = nextConfig;
 /** @type {import('next').NextConfig} */
