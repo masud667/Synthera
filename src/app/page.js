f1f1fa2038f700fa1c2ae197f33077dc5f285a39
Masud Parvaz <108426788+masud667@users.noreply.github.com>
2025-09-09 19:37:40 +0600

    fixed issue

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,4 @@
-import Hero from "./components/Hero";
+
 import ShopContent from "./components/products/ShopContent";
 import HeroBanner from "./components/HeroBanner";
 import Categories from "./components/home-compo/Category";
@@ -8,8 +8,6 @@ import OffersSection from "./components/OffersSection";
 export default function Home() {
   return (
     <>
-      
- 
       <HeroBanner />
        <ShopContent></ShopContent>
       <Categories></Categories>
