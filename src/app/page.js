98f7bdde8d4601c8e1c24774f69c6d33d64a2f3e
Masud Parvaz <108426788+masud667@users.noreply.github.com>
2025-09-11 11:54:38 +0600

    some issues fix

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,6 @@
 import ShopContent from "./components/products/ShopContent";
 import HeroBanner from "./components/HeroBanner";
 import Categories from "./components/home-compo/Category";
-import TrendingProducts from "./components/home-compo/TrendingProducts";
 import OffersSection from "./components/OffersSection";
 
 export default function Home() {
